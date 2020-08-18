c50ccfe095c19f7ccd373dcd004bfa23bb374d33
Atila Fassina <atila@fassina.eu>
2020-08-18 08:54:44 +0200

    add templates and more helpers

--- index.js
+++ index.js
@@ -2,12 +2,63 @@
 
 const prompts = require('prompts')
 const questions = require('./lib/questions')
-const { shell } = require('./lib/utils')
+const { shell, write, command } = require('./lib/utils')
+const pkgJsonTemplate = require('./templates/packageJson')
+const {
+  NETLIFY_DEPENDENCIES,
+  VERCEL_DEPENDENCIES,
+} = require('./lib/dependencies')
 
 ;(async () => {
   const CWD = process.cwd()
 
-  const response = await prompts(questions({ cwd: CWD }))
+  const {
+    packageName,
+    isPrivate,
+    withPrettier,
+    platform,
+    netlifyDev,
+    packageManager,
+  } = await prompts(questions({ cwd: CWD }))
+  await shell(`mkdir ${packageName}`)
+  const projectDir = `${CWD}/${packageName}`
+  const gitName = await shell(`git config --global user.name`)
+  const gitEmail = await shell(`git config --global user.email`)
+  const devInstall = command(packageManager, 'DEV_INSTALL')
 
-  shell(`yarn --help`)
+  await write(
+    projectDir,
+    pkgJsonTemplate({
+      platform,
+      packageName,
+      gitName,
+      gitEmail,
+      isPrivate,
+      withPrettier,
+    })
+  )
+
+  if (platform === 'netlify' && netlifyDev) {
+    const globalInstall = command(packageManager, 'GLOBAL_INSTALL')
+    await shell(`${packageManager} ${globalInstall} netlify-cli`)
+  }
+
+  switch (platform) {
+    case 'netlify':
+      if (withPrettier) NETLIFY_DEPENDENCIES.push('prettier')
+      await shell(
+        `${packageManager} ${devInstall} ${NETLIFY_DEPENDENCIES.join(' ')}`,
+        projectDir
+      )
+      break
+    case 'vercel':
+      if (withPrettier) VERCEL_DEPENDENCIES.push('prettier')
+      await shell(
+        `${packageManager} ${devInstall} ${VERCEL_DEPENDENCIES.join(' ')}`,
+        projectDir
+      )
+      break
+    default:
+      break
+  }
 })()
---
+++ templates/packageJson.js
@@ -0,0 +1,37 @@
+function createTasks(platform) {
+  return platform === 'netlify'
+    ? `"scripts": {
+    "build": "netlify-lambda build src",
+    "ts-check": "tsc --noEmit --lib ES2015 ./src/*.ts"
+  }
+  `
+    : ','
+}
+
+module.exports = ({
+  packageName,
+  gitName,
+  gitEmail,
+  isPrivate,
+  withPrettier,
+  platform,
+}) => `
+{
+  "name": "${packageName}",
+  "version": "1.0.0",
+  "main": "index.js",
+  "repository": "git@github.com:atilafassina/lambda-starter.git",
+  "author": "${gitName.trimEnd()} <${gitEmail.trimEnd()}>",
+  "license": "MIT",
+  "private": ${isPrivate},
+  ${createTasks(platform)}${
+  withPrettier &&
+  `,
+  "prettier": {
+    "semi": false,
+    "singleQuote": true
+  }`
+}
+}
+
+`
