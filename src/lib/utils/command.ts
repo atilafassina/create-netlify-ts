acdee3b8ca9bb6c1e94fdcc12197473925da1ea0
Ademílson F. Tonato <ademilsonft@outlook.com>
2020-08-19 23:19:23 +0100

    creating interface for command options

--- src/lib/utils/command.ts
+++ src/lib/utils/command.ts
@@ -1,12 +1,15 @@
-export function command(pkgManager: string, cmd: string) {
-  switch (cmd) {
-    case 'GLOBAL_INSTALL':
-      return pkgManager === 'yarn' ? 'global add --silent' : 'i -g'
-    case 'LOCAL_INSTALL':
-      return pkgManager === 'yarn' ? 'add --silent' : 'i'
-    case 'DEV_INSTALL':
-      return pkgManager === 'yarn' ? 'add -D --silent' : 'i -D'
-    default:
-      return '--help'
+interface ICommandOptions {
+  [key: string]: any;
+}
+
+export function command(pkgManager: string, cmd: string): string {
+  let _cmd = '--help'
+  
+  const options: ICommandOptions = {
+    'GLOBAL_INSTALL': pkgManager === 'yarn' ? 'global add --silent' : 'i -g',
+    'LOCAL_INSTALL': pkgManager === 'yarn' ? 'global add --silent' : 'i -g',
+    'DEV_INSTALL': pkgManager === 'yarn' ? 'global add --silent' : 'i -g',
   }
+  
+  return options[cmd] || _cmd;
 }
